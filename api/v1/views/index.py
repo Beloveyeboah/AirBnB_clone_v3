68f07d36cad42b79a39981a83070179ba3b1ea8b
beloveyeboah@gmail.com <beloveyeboah>
2024-04-29 21:28:12 +0000

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,9 @@
 """
 Index model holds the endpoint (route)
 """
-from api.v1.views import app_views, storage
+from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status/')
 def status():
